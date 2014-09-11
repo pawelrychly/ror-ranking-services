942498272ebeda9bf33d99084134b3f9bc7936fa
pawel <pawel@pawel-K50AB.(none)>
2014-09-12 00:14:54 +0200

    Modules renamed

---
+++ RORUTA-NecessaryAndPossiblePreferenceRelations-PUT/RORUTA-NecessaryAndPossiblePreferenceRelations.R
@@ -0,0 +1,198 @@
+##########################################
+# Usage:
+# R --slave --vanilla --args "[inDirectory]" "[outDirectory]" < RORUTA-NecessaryAndPossiblePreferenceRelations.R
+# Example: 
+# R --slave --vanilla --args "${PWD}/in" "${PWD}/out" < RORUTA-NecessaryAndPossiblePreferenceRelations.R
+##########################################
+
+inDirectory <- commandArgs()[5] 
+outDirectory <- commandArgs()[6]  
+#inDirectory <- "/home/pawel/Projects/ror-workspace/robust-ordinal-regresion/tests/in2" 
+#outDirectory <-"/home/pawel/Projects/ror-workspace/robust-ordinal-regresion/tests/out2" 
+##########################################
+# Set the working directory as the "in" directory
+##########################################
+setwd(inDirectory)
+options( java.parameters = "-Xmx2g" )
+library(rorranking)
+
+errFile<-NULL
+errData<-NULL
+errCalc<-NULL
+execFlag<-FALSE
+
+#INPUT FILES:
+alternatives.filename = "alternatives.xml"
+criteria.filename = "criteria.xml"
+performances.filename = "performances.xml"
+preferences.filename = "preferences.xml"
+characteristic.points.filename = "characteristic-points.xml"
+parameters.filename = "parameters.xml"
+rank.related.preferences.filename = "rank-related-requirements.xml"
+intensities.of.preferences.filename = "intensities-of-preferences.xml"
+
+#OUTPUT FILES:
+result.file.nec <- "necessary-relations.xml"
+result.file.pos <- "possible-relations.xml"
+result.file.messages <- "messages.xml"
+
+is_proper_data = TRUE
+performances <- rorranking:::getPerformancesFromXmcdaFiles(alternatives.filename=alternatives.filename,
+                                                                criteria.filename=criteria.filename,
+                                                                performances.filename=performances.filename)
+
+if (performances$status != "OK") {
+  errFile <- paste(errFile, performances$errFile)
+  errData <- paste(errData, performances$errData)
+  is_proper_data = FALSE
+} 
+#optional
+preferences = list("strong" = NULL, "weak" = NULL, "indif" = NULL)
+intensities.of.preferences =  list("strong" = NULL, "weak" = NULL, "indif" = NULL)
+rank.related.preferences = NULL
+nums.of.characteristic.points = NULL
+strict = FALSE
+if (is_proper_data) { #optional paramenters
+  intensities.of.preferences.data <- rorranking:::getIntensitiesOfPreferencesFromXmcdaFile(
+    filename=intensities.of.preferences.filename ,performances=performances$data)
+  if (intensities.of.preferences.data$status == "OK") {
+    intensities.of.preferences =  intensities.of.preferences.data$data
+  } else {
+      errData <- paste(errData,  intensities.of.preferences.data$errData)
+  }
+  
+  preferences.data <- rorranking:::getPreferencesFromXmcdaFile(filename=preferences.filename, performances=performances$data)
+  if (preferences.data$status == "OK") {
+    preferences = preferences.data$data
+  } else {
+    errData <- paste(errData,  preferences.data$errData)
+  }
+  
+  rank.related.preferences.data = rorranking:::getRankRelatedPreferencesFromXmcdaFile(rank.related.preferences.filename, performances$data)
+  if (rank.related.preferences.data$status == "OK") {
+    rank.related.preferences = rank.related.preferences.data$data
+  } else {
+    errData <- paste(errData,  rank.related.preferences.data$errData)
+  }
+  
+  characteristic.points.data <- rorranking:::getCharacteristicPointsFromXmcdaFile(filename=characteristic.points.filename,
+                                                                                  performances=performances$data)
+  
+  if (characteristic.points.data$status == "OK") {
+    nums.of.characteristic.points <- characteristic.points.data$data  
+  } else {
+    errData <- paste(errData,  characteristic.points.data$errData)
+  }
+  params.data <- rorranking:::getParametersDataFromXmcdaFile(filename=parameters.filename,
+                                                             keys=c("strict"), defaults=list("strict" = TRUE))
+  
+  if (params.data$status == "OK") {
+    strict <- params.data$data[['strict']]
+  } else {
+    errData <- paste(errData, params.data$errData)
+  }
+  
+}
+results <-NULL
+
+
+if (is.null(errFile) && is_proper_data){
+  tmpErr<- try (
+    {
+      results <- findNecessaryAndPossiblePreferenceRelations(
+        perf = performances$data, 
+        strong.prefs = preferences$strong, weak.prefs=preferences$weak, indif.prefs=preferences$indif,
+        strong.intensities.of.prefs = intensities.of.preferences$strong,
+        weak.intensities.of.prefs = intensities.of.preferences$weak,
+        indif.intensities.of.prefs = intensities.of.preferences$indif,
+        rank.related.requirements=rank.related.preferences,
+        strict.vf=strict, nums.of.characteristic.points=nums.of.characteristic.points) 
+    }, silent=TRUE
+  )  
+  if (inherits(tmpErr, 'try-error')){
+    errCalc<<-"Cannot find relations."
+  } else {
+    execFlag<-TRUE
+  }
+}
+
+setwd(outDirectory)
+if (execFlag) {
+  outTreeNec = newXMLDoc()
+  outTreePos = newXMLDoc()
+  newXMLNode("xmcda:XMCDA",
+             attrs=c("xsi:schemaLocation" = "http://www.decision-deck.org/2012/XMCDA-2.2.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.2.0.xsd"),
+             suppressNamespaceWarning=TRUE,
+             namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2012/XMCDA-2.2.0"),
+             parent=outTreeNec)
+  newXMLNode("xmcda:XMCDA",
+             attrs=c("xsi:schemaLocation" = "http://www.decision-deck.org/2012/XMCDA-2.2.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.2.0.xsd"),
+             suppressNamespaceWarning=TRUE,
+             namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2012/XMCDA-2.2.0"),
+             parent=outTreePos)
+  ids <- rownames(performances$data)
+  
+  nec.relations <- matrix(nrow=0, ncol=2)
+  pos.relations <- matrix(nrow=0, ncol=2)
+  for (id1 in ids) {
+    for (id2 in ids) {
+      if (results$nec.relations[id1,id2]) {
+        nec.rel <- c(id1, id2)
+        nec.relations <- rbind(nec.relations, nec.rel)
+      } 
+      if (results$pos.relations[id1,id2]) {
+        pos.rel <- c(id1, id2)
+        pos.relations <- rbind(pos.relations, pos.rel)
+      }
+    }
+  }
+ 
+  putAlternativesComparisonsLabels(outTreeNec, nec.relations)
+  putAlternativesComparisonsLabels(outTreePos, pos.relations)
+  saveXML(outTreeNec, file=result.file.nec)
+  saveXML(outTreePos, file=result.file.pos)
+
+  outTreeMessage = newXMLDoc()
+  newXMLNode("xmcda:XMCDA", 
+      attrs=c("xsi:schemaLocation" = "http://www.decision-deck.org/2009/XMCDA-2.0.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.0.0.xsd"),
+      suppressNamespaceWarning=TRUE, 
+      namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2009/XMCDA-2.0.0"), 
+      parent=outTreeMessage)
+
+  status<-putLogMessage(outTreeMessage, "OK", name = "executionStatus")
+  status<-saveXML(outTreeMessage, file=result.file.messages)
+}
+
+if (!is.null(errCalc)){
+  outTreeMessage = newXMLDoc()
+  newXMLNode("xmcda:XMCDA", 
+             attrs=c("xsi:schemaLocation" = "http://www.decision-deck.org/2012/XMCDA-2.2.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.2.0.xsd"),
+             suppressNamespaceWarning=TRUE, 
+             namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2012/XMCDA-2.2.0"), 
+             parent=outTreeMessage)
+  status<-putErrorMessage(outTreeMessage, errCalc, name="Error")
+  status<-saveXML(outTreeMessage, file=result.file.messages)
+  
+}
+
+if ((!is.null(errData)) && (length(errData) > 0)){
+  outTreeMessage = newXMLDoc()  
+  newXMLNode("xmcda:XMCDA", 
+             attrs=c("xsi:schemaLocation" = "http://www.decision-deck.org/2012/XMCDA-2.2.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.2.0.xsd"),
+             suppressNamespaceWarning=TRUE, 
+             namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2012/XMCDA-2.2.0"), 
+             parent=outTreeMessage)
+  status<-putErrorMessage(outTreeMessage, errData, name="Error")
+  status<-saveXML(outTreeMessage, file=result.file.messages)
+}
+
+if (!is.null(errFile)) {
+  outTreeMessage = newXMLDoc()
+  newXMLNode("xmcda:XMCDA", 
+             attrs=c("xsi:schemaLocation" = "http://www.decision-deck.org/2012/XMCDA-2.2.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.2.0.xsd"),
+             suppressNamespaceWarning=TRUE, 
+             namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2012/XMCDA-2.2.0"), 
+             parent=outTreeMessage)
+  status<-putErrorMessage(outTreeMessage, errFile, name="Error") 
+  status<-saveXML(outTreeMessage, file=result.file.messages)
+}
---
+++ RORUTA-PostFactum-PreferenceRelatedDeteriorationOrSurplusValueHierarchical-PUT/RORUTA-PostFactum-PreferenceRelatedDeteriorationOrSurplusValueHierarchical.R
@@ -0,0 +1,283 @@
+##########################################
+# Usage:
+# R --slave --vanilla --args "[inDirectory]" "[outDirectory]" < RORUTA-PostFactum-PreferenceRelatedDeteriorationOrSurplusValueHierarchical.R
+# Example: 
+# R --slave --vanilla --args "${PWD}/in" "${PWD}/out" < RORUTA-PostFactum-PreferenceRelatedDeteriorationOrSurplusValueHierarchical.R
+##########################################
+
+inDirectory <- commandArgs()[5] 
+outDirectory <- commandArgs()[6]  
+
+##########################################
+# Set the working directory as the "in" directory
+##########################################
+setwd(inDirectory)
+options( java.parameters = "-Xmx2g" )
+library(rorranking)
+
+errFile<-NULL
+errData<-NULL
+errCalc<-NULL
+execFlag<-FALSE
+
+#INPUT FILES:
+alternatives.filename = "alternatives.xml"
+criteria.filename = "criteria.xml"
+performances.filename = "performances.xml"
+target.filename = "target.xml"
+preferences.filename = "preferences.xml"
+hierarchy.of.criteria.filename = "hierarchy-of-criteria.xml"
+intensities.of.preferences.filename = "intensities-of-preferences.xml"
+rank.related.preferences.filename = "rank-related-requirements.xml"
+characteristic.points.filename = "characteristic-points.xml"
+selected.criteria.filename = "selected-criteria.xml"
+parameters.filename = "parameters.xml"
+
+#OUTPUT FILES:
+result.file <- "result.xml"
+result.file.messages <- "messages.xml"
+
+is_proper_data = TRUE
+performances <- rorranking:::getPerformancesFromXmcdaFiles(alternatives.filename=alternatives.filename,
+                                                                criteria.filename=criteria.filename,
+                                                                performances.filename=performances.filename)
+target <- rorranking:::getAlternativesComparisonFromXmcdaFile(filename=target.filename, performances=performances$data)
+hierarchy.data = rorranking:::getHierarchyOfCriteriaFromXmcdaFile(hierarchy.of.criteria.filename)
+
+
+target.variant.a <- NULL
+target.variant.b <- NULL
+if (performances$status != "OK") {
+  errFile <- paste(errFile, performances$errFile)
+  errData <- paste(errData, performances$errData)
+  is_proper_data = FALSE
+} 
+
+if (target$status != "OK") {
+  errFile <- paste(errFile, target$errFile)
+  errData <- paste(errData, target$errData)
+  is_proper_data = FALSE
+} else {
+  target.variant.a <- target$data[[1]]
+  target.variant.b <- target$data[[2]]
+} 
+if (hierarchy.data$status != "OK") {
+  errFile <- paste(errFile, hierarchy.data$errFile)
+  errData <- paste(errData, hierarchy.data$errData)
+  is_proper_data = FALSE
+} else {
+  hierarchy.data = hierarchy.data$data
+}
+
+preferences = list("strong" = NULL, "weak" = NULL, "indif" = NULL)
+intensities.of.preferences =  list("strong" = NULL, "weak" = NULL, "indif" = NULL)
+rank.related.preferences = NULL
+nums.of.characteristic.points = NULL
+selected.attributes = NULL
+is.possible = FALSE
+strict = FALSE
+type.of.result = "deterioration"  
+precision = 0.005
+possible.or.necessary <- "necessary"
+
+if (is_proper_data) { #optional paramenters
+  #preferences
+  preferences.data <- rorranking:::getPreferencesFromXmcdaFile(filename=preferences.filename, performances=performances$data)
+  if (preferences.data$status == "OK") {
+    preferences = preferences.data$data
+  } else {
+    errData <- paste(errData, preferences.data$errData)
+  }
+  #intensities of preferences
+  intensities.of.preferences.data <- rorranking:::getIntensitiesOfPreferencesFromXmcdaFile(
+    filename=intensities.of.preferences.filename ,performances=performances$data)
+  if (intensities.of.preferences.data$status == "OK") {
+    intensities.of.preferences =  intensities.of.preferences.data$data
+  } else {
+    errData <- paste(errData,  intensities.of.preferences.data$errData)
+  }
+  #rank related preferences data
+  rank.related.preferences.data = rorranking:::getRankRelatedPreferencesFromXmcdaFile(rank.related.preferences.filename, performances$data)
+  if (rank.related.preferences.data$status == "OK") {
+    rank.related.preferences = rank.related.preferences.data$data
+  } else {
+    errData <- paste(errData,  rank.related.preferences.data$errData)
+  }
+  #numbers of characteristic points
+  characteristic.points.data <- rorranking:::getCharacteristicPointsFromXmcdaFile(filename=characteristic.points.filename,
+                                                                                  performances=performances$data)
+  if (characteristic.points.data$status == "OK") {
+    nums.of.characteristic.points <- characteristic.points.data$data  
+  } else {
+    errData <- paste(errData,  characteristic.points.data$errData)
+  }
+  #params data
+  params.data <- rorranking:::getParametersDataFromXmcdaFile(filename=parameters.filename,
+                                                             keys=c("strict", "type_of_result", "precision", "possible_or_necessary"), defaults=list("strict" = TRUE))
+ 
+  if (params.data$status == "OK") {
+    if ('strict' %in% names(params.data$data)) {
+      strict <- params.data$data[['strict']]  
+    }
+    if ('possible_or_necessary' %in% names(params.data$data)) {
+      possible.or.necessary <- params.data$data[['possible_or_necessary']] 
+    }
+    if ('type_of_result' %in% names(params.data$data)) {
+      type.of.result <- params.data$data[['type_of_result']]  
+    }
+    if ('precision' %in% names(params.data$data)) {
+      precision <- params.data$data[['precision']]  
+    }
+  } else {
+    errData <- paste(errData,  params.data$errData)
+  }
+  #selected attributes
+  selected.attributes.data <- rorranking:::getSelectedCriteriaFromXmcdaFile(filename=selected.criteria.filename,
+                                                                            criteriaIDs=dimnames(performances$data)[[2]])
+  if (selected.attributes.data$status == "OK") {
+    selected.attributes <- selected.attributes.data$data  
+  } else {
+    errData <- paste(errData, selected.attributes.data$errData)
+  }
+}
+
+
+results <-NULL
+if (is.null(errFile) && is_proper_data){
+  tmpErr<- try (
+    {
+    if (possible.or.necessary == "necessary") {
+        if (type.of.result == "surplus-value") {
+          results <- findMissingOrRedundantUtilityValueForRelationHierarchical(
+            perf=performances$data, a = target.variant.a, b = target.variant.b,
+            strict.vf = strict, is.possibly.preffered=FALSE,  
+            strong.prefs = preferences$strong, weak.prefs=preferences$weak, 
+            indif.prefs=preferences$indif, 
+            strong.intensities.of.prefs = intensities.of.preferences$strong,
+            weak.intensities.of.prefs = intensities.of.preferences$weak, 
+            indif.intensities.of.prefs = intensities.of.preferences$indif,
+            rank.related.requirements = rank.related.preferences, 
+            nums.of.characteristic.points = nums.of.characteristic.points,
+            improvement=FALSE, hierarchy.data=hierarchy.data)
+        } else {
+         
+          results <- findPerformanceModificationForNecessaryRelationHierarchical(
+            perf=performances$data, a = target.variant.a, b = target.variant.b,
+            strict.vf = strict,
+            strong.prefs = preferences$strong, weak.prefs=preferences$weak, 
+            indif.prefs=preferences$indif, 
+            strong.intensities.of.prefs = intensities.of.preferences$strong, 
+            weak.intensities.of.prefs = intensities.of.preferences$weak,
+            indif.intensities.of.prefs = intensities.of.preferences$indif,
+            rank.related.requirements=rank.related.preferences, 
+            nums.of.characteristic.points=nums.of.characteristic.points,
+            precision = precision,
+            which.attributes = selected.attributes, 
+            greater.than.one = FALSE, hierarchy.data=hierarchy.data)
+        } 
+      } else {
+        if (type.of.result == "surplus-value") {
+          results <- findMissingOrRedundantUtilityValueForRelationHierarchical(
+            perf=performances$data, a = target.variant.a, b = target.variant.b,
+            strict.vf = strict, is.possibly.preffered=TRUE,  
+            strong.prefs = preferences$strong, weak.prefs=preferences$weak, 
+            indif.prefs=preferences$indif, 
+            strong.intensities.of.prefs = intensities.of.preferences$strong,
+            weak.intensities.of.prefs = intensities.of.preferences$weak, 
+            indif.intensities.of.prefs = intensities.of.preferences$indif,
+            rank.related.requirements = rank.related.preferences, 
+            nums.of.characteristic.points = nums.of.characteristic.points,
+            improvement=FALSE, hierarchy.data=hierarchy.data)
+        } else {
+          results <- findPerformanceModificationForPossibleRelationHierarchical(
+            perf=performances$data, a = target.variant.a, b = target.variant.b,
+            strict.vf = strict,
+            strong.prefs = preferences$strong, weak.prefs=preferences$weak, 
+            indif.prefs=preferences$indif, 
+            strong.intensities.of.prefs = intensities.of.preferences$strong, 
+            weak.intensities.of.prefs = intensities.of.preferences$weak,
+            indif.intensities.of.prefs = intensities.of.preferences$indif,
+            rank.related.requirements=rank.related.preferences, 
+            nums.of.characteristic.points=nums.of.characteristic.points,
+            precision = precision,
+            which.attributes = selected.attributes, 
+            greater.than.one = FALSE, hierarchy.data=hierarchy.data)
+        }
+      } 
+    }, silent=TRUE
+  )    
+  if (inherits(tmpErr, 'try-error')){
+    errCalc<<-"Can't find a result."
+  } else {
+    execFlag<-TRUE
+  }
+}
+setwd(outDirectory)
+if (execFlag) {
+  tree = newXMLDoc()
+  newXMLNode("xmcda:XMCDA",
+             attrs=c("xsi:schemaLocation" = "http://www.decision-deck.org/2012/XMCDA-2.2.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.2.0.xsd"),
+             suppressNamespaceWarning=TRUE,
+             namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2012/XMCDA-2.2.0"),
+             parent=tree)
+  target.variant.a <- which(rownames(performances$data) == target.variant.a)
+  for (nodeid in names(results)) {
+    if ((("status" %in% results[[nodeid]]) && (results[[nodeid]]$status != "OK")) || (is.null(results[[nodeid]]$result))) {
+      results[[nodeid]]$result = "model infeasible"
+    }
+    res <- results[[nodeid]]$result 
+  
+    alternative.value <- matrix(data=c(target.variant.a, res), ncol=2)
+    
+    if (is.character(res)) {
+      rorranking:::putAlternativesValuesWithAttributes(tree, alternative.value, rownames(performances$data), attributes=c("id"=nodeid), typeOfValues="label")
+    } else {
+      rorranking:::putAlternativesValuesWithAttributes(tree, alternative.value, rownames(performances$data), attributes=c("id"=nodeid), typeOfValues="real")
+    }
+  }
+  saveXML(tree, file=result.file)
+
+  outTreeMessage = newXMLDoc()
+  newXMLNode("xmcda:XMCDA", 
+      attrs=c("xsi:schemaLocation" = "http://www.decision-deck.org/2009/XMCDA-2.0.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.0.0.xsd"),
+      suppressNamespaceWarning=TRUE, 
+      namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2009/XMCDA-2.0.0"), 
+      parent=outTreeMessage)
+
+  status<-putLogMessage(outTreeMessage, "OK", name = "executionStatus")
+  status<-saveXML(outTreeMessage, file=result.file.messages)
+}
+
+if (!is.null(errCalc)){
+  outTreeMessage = newXMLDoc()
+  newXMLNode("xmcda:XMCDA", 
+             attrs=c("xsi:schemaLocation" = "http://www.decision-deck.org/2012/XMCDA-2.2.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.2.0.xsd"),
+             suppressNamespaceWarning=TRUE, 
+             namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2012/XMCDA-2.2.0"), 
+             parent=outTreeMessage)
+  status<-putErrorMessage(outTreeMessage, errCalc, name="Error")
+  status<-saveXML(outTreeMessage, file=result.file.messages)
+  
+}
+
+if ((!is.null(errData)) && (length(errData) > 0)){
+  outTreeMessage = newXMLDoc()  
+  newXMLNode("xmcda:XMCDA", 
+             attrs=c("xsi:schemaLocation" = "http://www.decision-deck.org/2012/XMCDA-2.2.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.2.0.xsd"),
+             suppressNamespaceWarning=TRUE, 
+             namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2012/XMCDA-2.2.0"), 
+             parent=outTreeMessage)
+  status<-putErrorMessage(outTreeMessage, errData, name="Error")
+  status<-saveXML(outTreeMessage, file=result.file.messages)
+}
+
+if (!is.null(errFile)) {
+  outTreeMessage = newXMLDoc()
+  newXMLNode("xmcda:XMCDA", 
+             attrs=c("xsi:schemaLocation" = "http://www.decision-deck.org/2012/XMCDA-2.2.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.2.0.xsd"),
+             suppressNamespaceWarning=TRUE, 
+             namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2012/XMCDA-2.2.0"), 
+             parent=outTreeMessage)
+  status<-putErrorMessage(outTreeMessage, errFile, name="Error") 
+  status<-saveXML(outTreeMessage, file=result.file.messages)
+}
